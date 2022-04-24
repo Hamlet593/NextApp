7213b8418df8ccbb0b109416bb3b5b2eb88a3516
Hamo Muradyan <hamomuradyan93@gmail.com>
2022-04-24 17:36:56 +0400

    Starting 8 lesson

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Head from 'next/head';
 const MyApp = ({ Component, pageProps }) => (
   <Layout>
     <Head>
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet"> /</link> 
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet" />
     </Head>
     <Component {...pageProps} />
   </Layout>
